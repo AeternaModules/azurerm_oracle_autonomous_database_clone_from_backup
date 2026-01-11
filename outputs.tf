edcd08a8351bc5a16cf249dcacb4a1840c4df4c0
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:56:51 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "oracle_autonomous_database_clone_from_backups" {
+  description = "All oracle_autonomous_database_clone_from_backup resources"
+  value       = azurerm_oracle_autonomous_database_clone_from_backup.oracle_autonomous_database_clone_from_backups
+}
+
